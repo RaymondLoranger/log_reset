ceb47fa2a05d8f15a7764a21d9ed5e0d3a61d4a3
RaymondLoranger <Raymond.Loranger@bell.net>
2021-01-20 14:44:22 -0500

    0.1.35

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Log.Reset.MixProject do
   def project do
     [
       app: :log_reset,
-      version: "0.1.34",
+      version: "0.1.35",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Log Reset",
@@ -48,8 +48,6 @@ defmodule Log.Reset.MixProject do
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
       {:file_only_logger, "~> 0.1"},
-      {:mix_tasks,
-       github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
       {:persist_config, "~> 0.4", runtime: false}
     ]
   end
