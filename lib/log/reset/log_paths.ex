fa1383424a7bfa2ae363a063fa83bea12927e476
RaymondLoranger <Raymond.Loranger@bell.net>
2022-01-03 10:03:51 -0500

    pre self send

---
+++ lib/log/reset/log_paths.ex
@@ -0,0 +1,85 @@
+defmodule Log.Reset.LogPaths do
+  @moduledoc """
+  A map assigning configured log paths to their log levels, and functions.
+  """
+
+  # Must be in that order given the common 'Log' part...
+  alias Log.Reset
+  alias Log.Reset.Log
+
+  @typedoc "A map assigning configured log paths to their log levels"
+  @type t :: %{Logger.level() => Path.t()}
+
+  @doc """
+  Resets the configured log files of the given `levels`.
+  """
+  @spec reset_logs(t, Reset.levels()) :: :ok
+  def reset_logs(config_paths, :all) do
+    config_paths |> Map.values() |> reset_logs()
+  end
+
+  def reset_logs(_config_paths, :none), do: :ok
+
+  def reset_logs(config_paths, levels) when is_list(levels) do
+    for {level, log_path} <- config_paths, level in levels do
+      log_path
+    end
+    |> reset_logs()
+  end
+
+  def reset_logs(_config_paths, _), do: :ok
+
+  @doc """
+  Creates a map assigning each configured log path to its log level.
+  """
+  @spec new :: t
+  def new do
+    for config <- log_configs(), into: %{} do
+      {config[:level], config[:path]}
+    end
+  end
+
+  ## Private functions
+
+  @spec reset_logs([Path.t()]) :: :ok
+  defp reset_logs(log_paths) do
+    log_paths
+    # Delete log files first...
+    |> Enum.map(&Task.async(fn -> reset_log(&1) end))
+    |> Enum.map(&Task.await/1)
+    # Process results second...
+    |> log_results()
+  end
+
+  @spec log_results([tuple]) :: :ok
+  defp log_results(results) do
+    Enum.reduce(results, :ok, fn
+      {:ok, log_path}, _acc ->
+        :ok = Log.info(:log_reset, {log_path, __ENV__})
+
+      {:error, reason, log_path}, _acc ->
+        :ok = Log.error(:log_not_reset, {log_path, reason, __ENV__})
+    end)
+  end
+
+  @spec reset_log(Path.t()) :: tuple
+  defp reset_log(log_path) do
+    log_path = Path.expand(log_path)
+
+    case File.rm(log_path) do
+      :ok -> {:ok, log_path}
+      {:error, :enoent} -> {:ok, log_path}
+      {:error, reason} -> {:error, reason, log_path}
+    end
+  end
+
+  @spec log_configs :: [Keyword.t()]
+  defp log_configs do
+    :application.get_env(:logger, :backends, [])
+    |> Enum.map(fn
+      {LoggerFileBackend, id} -> :application.get_env(:logger, id, nil)
+      _console -> nil
+    end)
+    |> Enum.reject(&is_nil/1)
+  end
+end
---
+++ lib/log/reset/log_paths/server.ex
@@ -0,0 +1,45 @@
+defmodule Log.Reset.LogPaths.Server do
+  @moduledoc """
+  A server process that holds, as its state, a map assigning configured log
+  paths to their log levels.
+  """
+
+  use GenServer
+
+  alias __MODULE__
+  alias Log.Reset.LogPaths
+  alias Log.Reset
+
+  @doc """
+  Spawns a "log paths" server process registered under the module name.
+  """
+  @spec start_link(Reset.levels()) :: GenServer.on_start()
+  def start_link(levels) do
+    GenServer.start_link(Server, levels, name: Server)
+  end
+
+  ## Callbacks
+
+  @spec init(Reset.levels()) :: {:ok, state :: LogPaths.t()}
+  def init(levels) do
+    log_paths = LogPaths.new()
+    :ok = LogPaths.reset_logs(log_paths, levels)
+    {:ok, log_paths}
+  end
+
+  @spec handle_call(atom | tuple, GenServer.from(), LogPaths.t()) ::
+          {:reply, reply :: term, state :: LogPaths.t()}
+  def handle_call(:log_paths, _from, log_paths) do
+    {:reply, log_paths, log_paths}
+  end
+
+  def handle_call(:refresh, _from, _log_paths) do
+    log_paths = LogPaths.new()
+    {:reply, log_paths, log_paths}
+  end
+
+  def handle_call({:reset_logs, levels}, _from, log_paths) do
+    :ok = LogPaths.reset_logs(log_paths, levels)
+    {:reply, :ok, log_paths}
+  end
+end
