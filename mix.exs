6cfd463e63abe4a37c00eba589d0613ebd3d4182
RaymondLoranger <Raymond.Loranger@bell.net>
2023-11-22 10:39:47 -0500

    0.1.56

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Log.Reset.MixProject do
   def project do
     [
       app: :log_reset,
-      version: "0.1.55",
+      version: "0.1.56",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Log Reset",
